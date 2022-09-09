c834582368845ac416f183065014b3b456f9120b
edchancdn <edchan.cdn@gmail.com>
2022-09-09 12:15:17 -0400

    added methods helloName, makeAbba, makeTags, makeOutWord in CodingbatString1 class

--- src/main/java/org/codesolutions/CodingbatString1.java
+++ src/main/java/org/codesolutions/CodingbatString1.java
@@ -1,4 +1,75 @@
 package org.codesolutions;
 
 public class CodingbatString1 {
+
+    public static void main(String[] args) {
+
+        /*
+        System.out.println(helloName("Bob"));
+        System.out.println(helloName("Alice"));
+        System.out.println(helloName("X"));
+
+        System.out.println(makeAbba("Hi", "Bye"));
+        System.out.println(makeAbba("Yo", "Alice"));
+        System.out.println(makeAbba("What", "Up"));
+
+        System.out.println(makeTags("i", "Yay"));
+        System.out.println(makeTags("i", "Hello"));
+        System.out.println(makeTags("cite", "Yay"));
+        */
+        System.out.println(makeOutWord("<<>>", "Yay"));
+        System.out.println(makeOutWord("<<>>", "WooHoo"));
+        System.out.println(makeOutWord("[[]]", "word"));
+    }
+
+    /* Given an "out" string length 4, such as "<<>>", and a word,
+    return a new string where the word is in the middle of the out string,
+    e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and
+    going up to but not including index j.
+    makeOutWord("<<>>", "Yay") → "<<Yay>>"
+    makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+    makeOutWord("[[]]", "word") → "[[word]]"
+     */
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2, 4);
+    }
+
+    /* The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text.
+    In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+    Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+    makeTags("i", "Yay") → "<i>Yay</i>"
+    makeTags("i", "Hello") → "<i>Hello</i>"
+    makeTags("cite", "Yay") → "<cite>Yay</cite>"
+     */
+    public static String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    /* Given two strings, a and b, return the result of putting them together in the order abba,
+    e.g. "Hi" and "Bye" returns "HiByeByeHi".
+    makeAbba("Hi", "Bye") → "HiByeByeHi"
+    makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+    makeAbba("What", "Up") → "WhatUpUpWhat"
+     */
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+    /* Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+    helloName("Bob") → "Hello Bob!"
+    helloName("Alice") → "Hello Alice!"
+    helloName("X") → "Hello X!"
+     */
+    public static String helloName(String name) {
+        String rtnVal = "Hello ";
+        rtnVal = rtnVal.concat(name).concat("!");
+        return rtnVal;
+    }
+    /* This is a variant of helloName() without the usage of variables and String.concat
+     */
+    public static String helloNameV2(String name) {
+        return "Hello " + name + "!";
+    }
+
+
 }
