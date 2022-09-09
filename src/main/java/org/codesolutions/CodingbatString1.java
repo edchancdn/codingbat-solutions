3e2754ce7422e28f1eceb8b42fa23764a4d98271
edchancdn <edchan.cdn@gmail.com>
2022-09-09 13:49:48 -0400

    added methods extraEnd, firstTwo, firstHalf, withoutEnd

--- src/main/java/org/codesolutions/CodingbatString1.java
+++ src/main/java/org/codesolutions/CodingbatString1.java
@@ -16,10 +16,93 @@ public class CodingbatString1 {
         System.out.println(makeTags("i", "Yay"));
         System.out.println(makeTags("i", "Hello"));
         System.out.println(makeTags("cite", "Yay"));
-        */
         System.out.println(makeOutWord("<<>>", "Yay"));
         System.out.println(makeOutWord("<<>>", "WooHoo"));
         System.out.println(makeOutWord("[[]]", "word"));
+
+        System.out.println(extraEnd("Hello"));
+        System.out.println(extraEnd("ab"));
+        System.out.println(extraEnd("Hi"));
+
+        System.out.println(firstTwo("Hello"));
+        System.out.println(firstTwo("abcdefg"));
+        System.out.println(firstTwo("z"));
+
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
+        */
+        System.out.println(withoutEnd("Hello"));
+        System.out.println(withoutEnd("java"));
+        System.out.println(withoutEnd("coding"));
+    }
+
+
+
+    /* Given a string, return a version without the first and last char,
+    so "Hello" yields "ell".
+    The string length will be at least 2.
+    withoutEnd("Hello") → "ell"
+    withoutEnd("java") → "av"
+    withoutEnd("coding") → "odin"
+     */
+    public static String withoutEnd(String str) {
+        return str.substring(1, str.length() - 1);
+    }
+
+    /* Given a string of even length, return the first half.
+    So the string "WooHoo" yields "Woo".
+    firstHalf("WooHoo") → "Woo"
+    firstHalf("HelloThere") → "Hello"
+    firstHalf("abcdef") → "abc"
+     */
+    public static String firstHalf(String str) {
+        return str.substring(0, str.length() / 2);
+    }
+
+
+    /* Given a string, return the string made of its first two chars,
+    so the String "Hello" yields "He". If the string is shorter than length 2,
+    return whatever there is,
+    so "X" yields "X", and the empty string "" yields the empty string "".
+    Note that str.length() returns the length of a string.
+    firstTwo("Hello") → "He"
+    firstTwo("abcdefg") → "ab"
+    firstTwo("ab") → "ab"
+     */
+    public static String firstTwo(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2);
+        } else {
+            return str;
+        }
+    }
+
+
+    /* Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+    The string length will be at least 2.
+    extraEnd("Hello") → "lololo"
+    extraEnd("ab") → "ababab"
+    extraEnd("Hi") → "HiHiHi"
+     */
+    public static String extraEnd(String str) {
+        String rtnVal = "";
+        if (str.length() >= 2) {
+            for (int i = 0; i < 3; i++) {
+                rtnVal = rtnVal.concat(str.substring(str.length() - 2));
+            }
+        }
+        return rtnVal;
+    }
+    /* This variant stores the last 2 chars in a variable and uses no loop, just simple concat.
+     */
+    public static String extraEndV2(String str) {
+        String rtnVal = "";
+        if (str.length() >= 2) {
+            String last2 = str.substring(str.length() - 2);
+            rtnVal = last2 + last2 + last2;
+        }
+        return rtnVal;
     }
 
     /* Given an "out" string length 4, such as "<<>>", and a word,
