54161bec0b921ee7c62cb57ed4d0f25beaeb9d00
edchancdn <edchan.cdn@gmail.com>
2022-09-12 08:50:39 -0400

    added methods endsLy and nTwice in CodingbatString1

--- src/main/java/org/codesolutions/CodingbatString1.java
+++ src/main/java/org/codesolutions/CodingbatString1.java
@@ -61,10 +61,46 @@ public class CodingbatString1 {
         System.out.println(withouEnd2("Hello"));
         System.out.println(withouEnd2("abc"));
         System.out.println(withouEnd2("ab"));
-        */
+
         System.out.println(middleTwo("string"));
         System.out.println(middleTwo("code"));
         System.out.println(middleTwo("Practice"));
+
+        System.out.println(endsLy("oddly"));
+        System.out.println(endsLy("y"));
+        System.out.println(endsLy("oddy"));
+        */
+        System.out.println(nTwice("Hello", 2));
+        System.out.println(nTwice("Chocolate", 3));
+        System.out.println(nTwice("Chocolate", 1));
+    }
+
+    /* Given a string and an int n, return a string made of the first and last n chars from the string.
+    The string length will be at least n.
+    nTwice("Hello", 2) → "Helo"
+    nTwice("Chocolate", 3) → "Choate"
+    nTwice("Chocolate", 1) → "Ce"
+     */
+    public static String nTwice(String str, int n) {
+        String rtnVal = "str";
+        if (str.length() >= n) {
+            rtnVal = str.substring(0,  n) + str.substring(str.length() - n);
+        }
+        return rtnVal;
+    }
+
+    /* Given a string, return true if it ends in "ly".
+    endsLy("oddly") → true
+    endsLy("y") → false
+    endsLy("oddy") → false
+     */
+    public static boolean endsLy(String str) {
+        if (str.length() >= 2) {
+            // get last 2 chars and convert to lower case, then check if equal to "ly".
+            return str.substring(str.length() - 2).toLowerCase().equals("ly");
+        } else {
+            return false;
+        }
     }
 
     /* Given a string of even length, return a string made of the middle two chars,
