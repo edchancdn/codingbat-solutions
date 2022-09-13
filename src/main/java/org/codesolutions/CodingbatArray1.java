5536a79b55da030e591853543b2feee5f0b37102
edchancdn <edchan.cdn@gmail.com>
2022-09-13 14:46:49 -0400

    added class CodingbatArray1

---
+++ src/main/java/org/codesolutions/CodingbatArray1.java
@@ -0,0 +1,13 @@
+package org.codesolutions;
+
+public class CodingbatArray1 {
+
+    public static void main(String[] args) {
+        /* Basic array problems -- no loops..
+        Use a[0], a[1], ...
+        to access elements in an array,
+        a.length is the length (note that s.length() is for Strings).
+        Allocate a new array like this: int[] a = new int[10]; // length 10 array
+         */
+    }
+}
