fb1d5a974563cd911a7e3d49e450cd41ee33a955
edchancdn <edchan.cdn@gmail.com>
2022-09-14 09:57:59 -0400

    added firstLast6 in CodingbatArray1

--- src/main/java/org/codesolutions/CodingbatArray1.java
+++ src/main/java/org/codesolutions/CodingbatArray1.java
@@ -1,7 +1,6 @@
 package org.codesolutions;
 
 public class CodingbatArray1 {
-
     public static void main(String[] args) {
         /* Basic array problems -- no loops..
         Use a[0], a[1], ...
@@ -9,5 +8,23 @@ public class CodingbatArray1 {
         a.length is the length (note that s.length() is for Strings).
         Allocate a new array like this: int[] a = new int[10]; // length 10 array
          */
+
+        System.out.println(firstLast6(new int[] {1, 2, 6}));
+        System.out.println(firstLast6(new int[] {6, 1, 2, 3}));
+        System.out.println(firstLast6(new int[] {12, 6, 1, 2, 3}));
+    }
+
+    /* Given an array of ints, return true if 6 appears as either the first or last element in the array.
+    The array will be length 1 or more.
+    firstLast6([1, 2, 6]) → true
+    firstLast6([6, 1, 2, 3]) → true
+    firstLast6([13, 6, 1, 2, 3]) → false
+     */
+    public static boolean firstLast6(int[] nums) {
+        boolean rtnVal = false;
+        if (nums[0] == 6 || nums[nums.length - 1] == 6) {
+            rtnVal = true;
+        }
+        return rtnVal;
     }
 }
