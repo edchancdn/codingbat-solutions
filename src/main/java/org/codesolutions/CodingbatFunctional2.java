8d41f30a9bad7fa66c204b3e1a97669b98adc474
edchancdn <edchan.cdn@gmail.com>
2022-10-26 10:17:19 -0400

    added methods to CodingbatFunctional2

--- src/main/java/org/codesolutions/CodingbatFunctional2.java
+++ src/main/java/org/codesolutions/CodingbatFunctional2.java
@@ -14,19 +14,60 @@ public class CodingbatFunctional2 {
         The main computation for each of these problems can be done with 1 or 2 lines of lambda code.
          */
 
-        List<Integer> ln = new ArrayList<>(
-                Arrays.asList(1, 29, 14, 569));
-        List<Integer> lnn = new ArrayList<>(
-                Arrays.asList(1, -3, 0, 22, 3, 564));
+        List<Integer> ln = new ArrayList<>(Arrays.asList(1, 29, 14, 569, 4, 4));
+        List<Integer> lnn = new ArrayList<>(Arrays.asList(1, -3, 0, 22, 3, 564));
         List<String> ls = new ArrayList<>(
-                Arrays.asList("hi", "Hello", "ABC", "xyz"));
+                Arrays.asList("hi", "Hello", "ABC", "xyz", "cy"));
 
-        System.out.println(noNeg(lnn));
-        System.out.println(no9(ln));
-        System.out.println(noTeen(ln));
-        System.out.println(noZ(ls));
-        System.out.println(noLong(ls));
-        System.out.println(no34(ls));
+        CodingbatFunctional2 cb = new CodingbatFunctional2();
+        System.out.println(cb.noNeg(lnn));
+        System.out.println(cb.no9(ln));
+        System.out.println(cb.noTeen(ln));
+        System.out.println(cb.noZ(ls));
+        System.out.println(cb.noLong(ls));
+        System.out.println(cb.no34(ls));
+        System.out.println(cb.noYY(ls));
+        System.out.println(cb.two2(ln));
+        System.out.println(cb.square56(ln));
+    }
+
+
+    /*
+    Given a list of integers, return a list of those numbers squared and the product added to 10,
+    omitting any of the resulting numbers that end in 5 or 6.
+
+    square56([3, 1, 4]) → [19, 11]
+    square56([1]) → [11]
+    square56([2]) → [14]
+     */
+    public List<Integer> square56(List<Integer> nums) {
+        return nums.stream().map(n -> (n * n) + 10).filter(n -> n % 10 != 5 && n % 10 != 6)
+                .collect(Collectors.toList());
+    }
+
+    /*
+    Given a list of non-negative integers, return a list of those numbers multiplied by 2,
+    omitting any of the resulting numbers that end in 2.
+
+    two2([1, 2, 3]) → [4, 6]
+    two2([2, 6, 11]) → [4]
+    two2([0]) → [0]
+     */
+    public List<Integer> two2(List<Integer> nums) {
+        return nums.stream().map(n -> n * 2).filter(n -> n % 10 != 2).collect(Collectors.toList());
+    }
+
+    /*
+    Given a list of strings, return a list where each string has "y" added at its end,
+    omitting any resulting strings that contain "yy" as a substring anywhere.
+
+    noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+    noYY(["a", "b", "cy"]) → ["ay", "by"]
+    noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]
+     */
+    public List<String> noYY(List<String> strings) {
+        return strings.stream().map(s -> s + "y")
+                .filter(s -> !s.contains("yy")).collect(Collectors.toList());
     }
 
     /*
@@ -36,7 +77,7 @@ public class CodingbatFunctional2 {
     no34(["a", "bb", "ccc", "dddd"]) → ["a", "bb"]
     no34(["ccc", "dddd", "apple"]) → ["apple"]
      */
-    public static List<String> no34(List<String> strings) {
+    public List<String> no34(List<String> strings) {
         return strings.stream().filter(s -> s.length() < 3 || s.length() > 4).collect(Collectors.toList());
     }
 
@@ -47,7 +88,7 @@ public class CodingbatFunctional2 {
     noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
     noLong(["cccc", "cccc", "cccc"]) → []
      */
-    public static List<String> noLong(List<String> strings) {
+    public List<String> noLong(List<String> strings) {
         return strings.stream().filter(n -> n.length() < 4).collect(Collectors.toList());
     }
 
@@ -59,7 +100,7 @@ public class CodingbatFunctional2 {
     noZ(["hziz", "hzello", "hi"]) → ["hi"]
     noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
      */
-    public static List<String> noZ(List<String> strings) {
+    public List<String> noZ(List<String> strings) {
         return strings.stream().filter(s -> s.contains("z") == false).collect(Collectors.toList());
     }
 
@@ -70,7 +111,7 @@ public class CodingbatFunctional2 {
     noTeen([1, 14, 1]) → [1, 1]
     noTeen([15]) → []
      */
-    public static List<Integer> noTeen(List<Integer> nums) {
+    public List<Integer> noTeen(List<Integer> nums) {
         return nums.stream().filter(n -> n < 13 || n > 19).collect(Collectors.toList());
     }
 
@@ -83,7 +124,7 @@ public class CodingbatFunctional2 {
     no9([9, 19, 29, 3]) → [3]
     no9([1, 2, 3]) → [1, 2, 3]
      */
-    public static List<Integer> no9(List<Integer> nums) {
+    public List<Integer> no9(List<Integer> nums) {
         return nums.stream().filter(n -> (n % 10) != 9).collect(Collectors.toList());
     }
 
@@ -94,7 +135,7 @@ public class CodingbatFunctional2 {
     noNeg([-3, -3, 3, 3]) → [3, 3]
     noNeg([-1, -1, -1]) → []
      */
-    public static List<Integer> noNeg(List<Integer> nums) {
+    public List<Integer> noNeg(List<Integer> nums) {
         return nums.stream().filter(n -> n >= 0).collect(Collectors.toList());
     }
 }
